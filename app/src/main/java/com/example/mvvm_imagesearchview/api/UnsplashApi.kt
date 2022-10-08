a541f7b16d431096c5bbce23506ecae0a16e7d02
Mohit Singh <singhmohit9718@gmail.com>
2022-10-09 00:25:52 +0530

    7th video

--- app/src/main/java/com/example/mvvm_imagesearchview/api/UnsplashApi.kt
+++ app/src/main/java/com/example/mvvm_imagesearchview/api/UnsplashApi.kt
@@ -2,7 +2,7 @@ package com.example.mvvm_imagesearchview.api
 
 
 
-import com.example.mvvm_imagesearchview.BuildConfig
+import androidx.viewbinding.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -13,7 +13,8 @@ interface UnsplashApi {
 
     companion object {
         const val BASE_URL = "https://api.unsplash.com/"
-        const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+
+        const val CLIENT_ID = "cFZNU4NnKCbBguaJTusLGUuEtB0doJSPf-zUQOoefq4"
     }
     @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
     @GET("search/photos")
--- app/src/main/java/com/example/mvvm_imagesearchview/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/mvvm_imagesearchview/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mvvm_imagesearchview.R
+import com.example.mvvm_imagesearchview.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,12 +13,29 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel> ()
 
+    private var _binding:FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        _binding = FragmentGalleryBinding.bind(view)
 
+        val adapter = UnsplashPhotoAdapter()
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle,it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
