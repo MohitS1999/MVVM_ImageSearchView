8806f04ec529852bcfafa92da7713fcf88f033fb
Mohit Singh <singhmohit9718@gmail.com>
2022-10-08 13:40:23 +0530

    4th video

---
+++ app/src/main/java/com/example/mvvm_imagesearchview/api/UnsplashApi.kt
@@ -0,0 +1,26 @@
+package com.example.mvvm_imagesearchview.api
+
+
+
+import com.example.mvvm_imagesearchview.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+        const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+    }
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+
+    ): UnsplashRespose
+
+}
---
+++ app/src/main/java/com/example/mvvm_imagesearchview/api/UnsplashRespose.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_imagesearchview.api
+
+import com.example.mvvm_imagesearchview.data.UnsplashPhoto
+
+data class UnsplashRespose (
+    val results: List<UnsplashPhoto>
+)
